220cd8ac7527e3748dcb43157f4bc3afd322b115
GabrielBauer19 <gabrielbauer19082008@gmail.com>
2026-06-28 01:38:02 -0300

    exercicio usando for

--- aula-06/aula-listadealunos.js
+++ aula-06/aula-listadealunos.js
@@ -1,7 +1,14 @@
 let alunos = [
 {nome: 'ana', idade: 18, curso: 'fullstack'},
-{nome: 'joao', idade: 20, curso: 'fullstack'}
+{nome: 'joao', idade: 20, curso: 'fullstack'},
+{nome: 'maria', idade: 27, curso: 'fullstack'}
+];
+console.log("=====usando for=====");
+
+for (let i = 0; i < alunos.length; i++) {
+    
+console.log("Nome:",alunos[i].nome + " | " + "Idade:",alunos[i].idade);
 
 
+}
 
-]
